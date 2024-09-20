e1592e8c6e9f4fcedcdc9e20602335eeec28fe37
Krutika Bhadane <krutikabhadane79@gmail.com>
2024-09-20 22:55:15 +0530

    Added: UC-3 Given 3 strings find the maximum string

--- src/main/java/com/bridgelabz/generic/TestMaximum.java
+++ src/main/java/com/bridgelabz/generic/TestMaximum.java
@@ -30,6 +30,20 @@ public class TestMaximum
         System.out.println("Maximum integer is " + max+" and its position is "+position);
     }
 
+    public static void getMaximumString(String[] arr) {
+
+        String max = arr[0];
+        int position = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int a = arr[i].compareTo(max);
+            if (a > 0) {
+                max = arr[i];
+                position = i;
+            }
+        }
+        System.out.println("Maximum string is " + max+" and its position is "+position);
+    }
+
     public static void main( String[] args )
     {
         Integer[] arr = {10, 40, 20};
@@ -37,5 +51,8 @@ public class TestMaximum
 
         Float[] floatArr = {2.3f,4.2f,7.5f};
         getMaximumFloat(floatArr);
+
+        String[] stringArr = {"Krutika", "Kajal", "Kavya"};
+        getMaximumString(stringArr);
     }
 }
