388e07a5ccb25e1ae234efd0623526bd74cdd0b0
Krutika Bhadane <krutikabhadane79@gmail.com>
2024-09-20 23:01:07 +0530

    Refactor: All the 3 to One Generic Method and find the maximum

--- src/main/java/com/bridgelabz/generic/TestMaximum.java
+++ src/main/java/com/bridgelabz/generic/TestMaximum.java
@@ -44,15 +44,33 @@ public class TestMaximum
         System.out.println("Maximum string is " + max+" and its position is "+position);
     }
 
+    public static <T extends Comparable<T>> void getMax(T[] arr) {
+        T max = arr[0];
+        int position = 0;
+        for (int i = 0; i < arr.length; i++)
+        {
+            int a = arr[i].compareTo(max);
+            if(a > 0)
+            {
+                max = arr[i];
+                position = i;
+            }
+        }
+        System.out.println("Maximum element is " + max+" and its position is "+position);
+    }
+
     public static void main( String[] args )
     {
         Integer[] arr = {10, 40, 20};
         getMaximumInteger(arr);
+        getMax(arr);
 
         Float[] floatArr = {2.3f,4.2f,7.5f};
         getMaximumFloat(floatArr);
+        getMax(floatArr);
 
         String[] stringArr = {"Krutika", "Kajal", "Kavya"};
         getMaximumString(stringArr);
+        getMax(stringArr);
     }
 }
