369f2fe5d097ca8977495099b2b4b6e2f170531d
Krutika Bhadane <krutikabhadane79@gmail.com>
2024-09-20 22:46:33 +0530

    Added: UC-1 Given 3 integers find the maximum integer

---
+++ src/main/java/com/bridgelabz/generic/TestMaximum.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.generic;
+
+public class TestMaximum
+{
+    public static void getMaximumInteger(Integer[] arr) {
+
+        int max = arr[0];
+        int position = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int a = arr[i].compareTo(max);
+            if (a > 0) {
+                max = arr[i];
+                position = i;
+            }
+        }
+        System.out.println("Maximum integer is " + max+" and its position is "+position);
+    }
+
+    public static void main( String[] args )
+    {
+        Integer[] arr = {10, 40, 20};
+        getMaximumInteger(arr);
+    }
+}
