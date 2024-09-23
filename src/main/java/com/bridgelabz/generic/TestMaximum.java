61c715f4c0bb742dabca3920a1b98ce503d6f972
Krutika Bhadane <krutikabhadane79@gmail.com>
2024-09-23 11:03:29 +0530

    Added: UC-4 Extend the max method to take more than three parameters

--- src/main/java/com/bridgelabz/generic/TestMaximum.java
+++ src/main/java/com/bridgelabz/generic/TestMaximum.java
@@ -1,7 +1,18 @@
 package com.bridgelabz.generic;
 
-public class TestMaximum
-{
+import java.util.Arrays;
+
+public class TestMaximum<T> {
+    T[] typeArray;
+    public TestMaximum(T[] typeArray)
+    {
+        this.typeArray = typeArray;
+    }
+    public TestMaximum()
+    {
+
+    }
+
     public static void getMaximumInteger(Integer[] arr) {
 
         int max = arr[0];
@@ -44,33 +55,47 @@ public class TestMaximum
         System.out.println("Maximum string is " + max+" and its position is "+position);
     }
 
-    public static <T extends Comparable<T>> void getMax(T[] arr) {
-        T max = arr[0];
+    public static <T extends Comparable<T>> void getMax(TestMaximum testMaximum) {
+        Arrays.sort(testMaximum.typeArray);
+        T max = (T) testMaximum.typeArray[0];
         int position = 0;
-        for (int i = 0; i < arr.length; i++)
+        for (int i = 0; i < testMaximum.typeArray.length; i++)
+
         {
-            int a = arr[i].compareTo(max);
-            if(a > 0)
+            T a = (T)testMaximum.typeArray[i];
+            int b = a.compareTo(max);
+            if(b > 0)
             {
-                max = arr[i];
+                max = (T) testMaximum.typeArray[i];
                 position = i;
             }
         }
-        System.out.println("Maximum element is " + max+" and its position is "+position);
+        System.out.println("Maximum element is : "+max);
+        System.out.println("Maximum string is " + max+" and its position is "+position);
+        printArray(testMaximum.typeArray);
+
+    }
+
+    public static void printArray(Object[] typeArray) {
+        for (Object element : typeArray)
+        {
+            System.out.println(element+"  ");
+        }
     }
 
     public static void main( String[] args )
     {
         Integer[] arr = {10, 40, 20};
         getMaximumInteger(arr);
-        getMax(arr);
 
         Float[] floatArr = {2.3f,4.2f,7.5f};
         getMaximumFloat(floatArr);
-        getMax(floatArr);
 
-        String[] stringArr = {"Krutika", "Kajal", "Kavya"};
+        String[] stringArr = {"Krutika", "Janhvi", "Kavya"};
         getMaximumString(stringArr);
-        getMax(stringArr);
+
+        getMax(new TestMaximum(arr));
+        getMax(new TestMaximum(floatArr));
+        getMax(new TestMaximum(stringArr));
     }
 }
